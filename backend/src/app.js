9810c8441f219634a4a35a1f436278b7a31d070c
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-23 22:50:30 +0530

    server js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req,res) => {
-    // res.send("Hello World");
+    res.send("Hello World");
 })
 
 // module.exports = app;
