309c8c3268d4c692b21b4df81958bc1401d263e1
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-23 22:49:02 +0530

    server js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 // const app = require('./src/app');
 // const connectDB = require('./src/db/db')
 
-connectDB();
+// connectDB();
 
 app.listen(3000,()=>{
     console.log("Server is running at port 3000");
