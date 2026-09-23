10b6b0a109218d75421a9dfedc2c3f62e0efa77f
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-23 22:49:34 +0530

    server js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@
 
 // connectDB();
 
-// app.listen(3000,()=>{
-    // console.log("Server is running at port 3000");
-// })
+app.listen(3000,()=>{
+    console.log("Server is running at port 3000");
+})
