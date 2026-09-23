cd0ca276afafc2a1d375072d57eab2311e646a31
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-23 22:53:25 +0530

    server js

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 function connectDB(){
     mongoose.connect("mongodb://localhost:27017/food-view")
-    // .then(()=>{
+    .then(()=>{
         // console.log("MongoDB connected");  
-    // })
+    })
     // .catch((err) => {
         // console.log("MongoDB connection error:",err);
     // })
